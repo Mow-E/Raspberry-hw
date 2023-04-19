356799415015b05efced5cd0bb2e9003fa376dc7
Anestis Cheimonettos <white.house@sitsena.xyz>
2023-04-20 01:32:39 +0200

    created scripts for testing websockets on backend

---
+++ websocket_client.py
@@ -0,0 +1,55 @@
+import datetime
+import json
+import time
+
+from stomp_ws.client import Client
+
+STATE_START = 0
+STATE_WORK = 1
+STATE_END = 2
+STATE_ERROR = 3
+STATE_COLLISION = 4
+
+
+def response(x, y, state):
+    return json.dumps(
+        {
+            'mowerId': "e193c17a-9c4e-4e3b-b2bc-f7a8a31a42b0",
+            'x': x,
+            'y': y,
+            'time': int(datetime.datetime.now().timestamp()),
+            'state': state
+        }
+    )
+
+
+if __name__ == '__main__':
+
+    # open transport
+    client = Client("ws://127.0.0.1:8080/websocket")
+
+    # connect to the endpoint
+    client.connect(
+        #     login="name",
+        #     passcode="45C82C421EBA87C8131E220F878E4145",
+        timeout=0)
+
+    x = 1
+    y = 2
+
+    client.send("/app/coordinate", body=response(x, y, STATE_START))
+
+    time.sleep(2)
+
+    for i in range(5):
+        time.sleep(1)
+        x = x + 1
+        y = y + 1
+        # send msg to channel
+        client.send("/app/coordinate", body=response(x, y, STATE_WORK))
+
+    time.sleep(1)
+
+    client.send("/app/coordinate", body=response(x, y, STATE_END))
+
+    client.disconnect()
