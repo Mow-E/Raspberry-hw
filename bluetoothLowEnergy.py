8b6e5ef94f1b7a1b7d1a1f38d9f77f401cda88b3
boorre96 <bobo20zr@student.ju.se>
2023-05-02 10:55:49 +0200

    Added comments in the code

--- bluetoothLowEnergy.py
+++ bluetoothLowEnergy.py
@@ -8,13 +8,14 @@ class bluetoothLoPo:
         # Create a bluetooth instance of Bleno()
         self.bleno = Bleno()
         
-    # This creates a callback function that will be called when the state of the instance is changed.
+    # This creates a callback function that will be called when the state of the instance is changed. "" is the instance and self. is the callbacks function
     def setup(self):
         self.bleno.on("stateChange", self.onstateChange)
         self.bleno.on("advertisingStart", self.onAdvertisingStart)
         self.bleno.on("accept", self.onAccept)
         self.bleno.on("disconnect", self.onDisconnect)
 
+    # This will check if the BLE radio is powered on, if so it will start to to advertise.
     def onstateChange(self, state):
         print(f"on -> stateChange: {state}")
 
@@ -22,8 +23,12 @@ class bluetoothLoPo:
             self.bleno.startAdvertising("Rasberry pi bleno", ["ec00"])
         else:
             self.bleno.stopAdvertising()
-
+            
+    # This will be triggered when the advertising is on or off.
     def onAdvertisingStart(self,error):
+
+        # If no error then it sets the service provided by the BLE pheripheral.
+        # For example when a app connects to the raspberry pi it can interract with this service.
         if not error:
             print("Advertising started successfully.")
             self.bleno.setServices([
@@ -37,12 +42,15 @@ class bluetoothLoPo:
         else:
             print(f"Advertising failed to start. Error: {error}")
 
+    # When a device is connected it will print its adress.
     def onAccept(self, clientAddress):
         print(f"Device connected: {clientAddress}")
 
+    # When a device is disconnected it will print its adress.
     def onDisconnect(self, clientAddress):
         print(f"Device disconnected: {clientAddress}")
     
+    # Starts the bluetooth module 
     def start(self):
         self.setup()
         self.bleno.start()
@@ -52,8 +60,6 @@ class bluetoothLoPo:
     
     def disconnect(self):
         self.bleno.disconnect()
-    def end(self):
-        self.bleno.end()
 
 ble = bluetoothLoPo()
 ble.start()
